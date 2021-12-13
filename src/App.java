29381b8df0e1c23b0f03f76b969e4a2c79a1c6bd
PXIONG001 <pengxiong001@gmail.com>
2021-12-12 19:58:13 -0600

    Creating a continous cycle of the user's experience.

--- src/App.java
+++ src/App.java
@@ -17,93 +17,108 @@ public class App {
      */
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
-        
+        Scanner sc = new Scanner(System.in);    
+        //Checking checking = new Checking(43263.90);    
+        Savings saving_total = new Savings(3023.01);
+        Boolean loop = true;
+
         System.out.println("Please enter your name to identify yourself! \n");
         
         System.out.println("Welcome " + sc.nextLine() + "! Which account would you like to access today?\n");
 
-        System.out.println("Checking\n\nSavings\n");
-        
-        String user_input = sc.nextLine();
+        while (loop) {
 
-        switch (user_input) {
-            
-            case "Checking":
-                Checking checking = new Checking(43263.90);
+            System.out.println("Checking\n\nSavings\n\nExit\n");
+            String user_input = sc.next();
+            System.out.println("Check the problem from above!");
 
-                System.out.println("What would you like to do today?\n");
-                System.out.println("1. Withdraw\n\n2. Deposit\n\n3. See Balance\n");
-                System.out.println("Type in the number:");
+            switch (user_input) {
+                /** 
+                case "Checking":
+                    
+
+                    System.out.println("What would you like to do today?\n");
+                    System.out.println("1. Withdraw\n\n2. Deposit\n\n3. See Balance\n");
+                    System.out.println("Type in the number:");
+                    
+                    String user_input_for_checking = sc.nextLine();
+                    
+                    switch (user_input_for_checking) {
+                        case "Withdraw":
+                        
+                            System.out.println("Bitch!");
+                            
+                            break;
+
+                        case "Deposit":
+                        
+                            System.out.println("Bitch!");
+                            
+                            break;
+
+                        case "See Balance":
+                        
+                            System.out.println("Balance: " + checking.getCheckingBalance());
+                            
+                            break;
+                    }
+
+                    break;
+                    */
+                case "Savings":
+                    
                 
-                String user_input_for_checking = sc.nextLine();
+                    System.out.println("What would you like to do today?\n");
+                    System.out.println("1. Withdraw\n\n2. Deposit\n\n3. See Balance\n");
+                    System.out.println("Type in the number:");
                 
-                switch (user_input_for_checking) {
-                    case "Withdraw":
-                    
-                        System.out.println("Bitch!");
+                    Integer user_input_for_savings = sc.nextInt();
+                
+                    switch (user_input_for_savings) {
+                        case 1:
+                            System.out.println("\nHow much would you like to withdraw?\n");
+                            
+                            Double user_withdraw = sc.nextDouble();
+                            Double withdraw = saving_total.getSavingsBalance() - user_withdraw;
+                            saving_total.setSavingsBalance(withdraw);
+
+                            System.out.println("You have withdrawn " + user_withdraw + ". You now have " + saving_total.getSavingsBalance());
+                            
+                            break;
+
+                        case 2:
+                            System.out.println("\nHow much would you like to deposit?\n");
+                            
+                            Double user_deposit = sc.nextDouble();
+                            Double deposit = saving_total.getSavingsBalance() + user_deposit;
+                            saving_total.setSavingsBalance(deposit);
                         
-                        break;
+                            System.out.println("You have deposited $" + user_deposit + ". You now have $" + saving_total.getSavingsBalance());
 
-                    case "Deposit":
-                    
-                        System.out.println("Bitch!");
+                            break;
+
+                        case 3:
                         
-                        break;
+                            System.out.println("Balance: " + saving_total.getSavingsBalance());
+                            
+                            break;
 
-                    case "See Balance":
-                    
-                        System.out.println("Balance: " + checking.getCheckingBalance());
-                        
-                        break;
-                }
+                        default:
+                            System.out.println("Don't fucking waste my time!");
+                            break;
+                    }
 
-                break;
+                    break;
 
-            case "Savings":
-                Savings saving_total = new Savings(3023.01);
-            
-                System.out.println("What would you like to do today?\n");
-                System.out.println("1. Withdraw\n\n2. Deposit\n\n3. See Balance\n");
-                System.out.println("Type in the number:");
-            
-                String user_input_for_savings = sc.nextLine();
-            
-                switch (user_input_for_savings) {
-                    case "Withdraw":
-                        System.out.println("\nHow much would you like to withdraw?\n");
-                         
-                        Double user_withdraw = sc.nextDouble();
-                        Double withdraw = saving_total.getSavingsBalance() - user_withdraw;
-                        saving_total.setSavingsBalance(withdraw);
+                case "Exit":
+                    loop = false;
+                    break;
 
-                        System.out.println("You have withdrawn " + withdraw + ". You now have " + saving_total.getSavingsBalance());
-                        
-                        break;
+                default:
+                    System.out.println("Check problem above!");
+                    break;
+            }
 
-                    case "Deposit":
-                        System.out.println("\nHow much would you like to deposit?\n");
-                    
-                        System.out.println("You have deposited ");
-                        
-                        break;
-
-                    case "See Balance":
-                    
-                        System.out.println("Balance: " + saving_total.getSavingsBalance());
-                        
-                        break;
-
-                    default:
-                        System.out.println("Don't fucking waste my time!");
-                        break;
-                }
-
-                break;
-
-            default:
-                System.out.println("So much for fucking wasting my time!");
-                break;
         }
 
         sc.close();
