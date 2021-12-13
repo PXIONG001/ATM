b702641fa2a4a463a3543caa189e3c78149ae422
PXIONG001 <pengxiong001@gmail.com>
2021-12-13 10:55:55 -0600

    Added a switch case with the checking account. There will be more remodifications.

--- src/App.java
+++ src/App.java
@@ -20,7 +20,7 @@ public class App
     {
         
         Scanner sc = new Scanner(System.in);    
-        //Checking checking = new Checking(43263.90);    
+        Checking checking = new Checking(43263.90);    
         Savings saving_total = new Savings(3023.01);
         Boolean loop = true;
 
@@ -37,6 +37,60 @@ public class App
             String user_input = sc.next();
 
             switch (user_input) {
+
+                case "Checking":
+                    Boolean loop_1 = true;
+                    
+                    while (loop_1) 
+                    {
+                        System.out.println("What would you like to do today?\n");
+                        System.out.println("1. Withdraw\n\n2. Deposit\n\n3. See Balance\n\n4. Exit\n");
+                        System.out.println("Type in the number:");
+                    
+                        Integer user_input_for_savings = sc.nextInt();
+
+                        switch (user_input_for_savings) 
+                        {
+                            case 1:
+                                System.out.println("\nHow much would you like to withdraw?\n");
+                                
+                                Double user_withdraw = sc.nextDouble();
+                                Double withdraw = checking.getCheckingBalance() - user_withdraw;
+                                saving_total.setSavingsBalance(withdraw);
+    
+                                System.out.println("You have withdrawn " + user_withdraw + ". You now have " + checking.getCheckingBalance());
+                                
+                                break;
+    
+                            case 2:
+                                System.out.println("\nHow much would you like to deposit?\n");
+                                
+                                Double user_deposit = sc.nextDouble();
+                                Double deposit = saving_total.getSavingsBalance() + user_deposit;
+                                saving_total.setSavingsBalance(deposit);
+                            
+                                System.out.println("You have deposited $" + user_deposit + ". You now have $" + saving_total.getSavingsBalance());
+    
+                                break;
+    
+                            case 3:
+                            
+                                System.out.println("Balance: " + checking.getCheckingBalance());
+                                
+                                break;
+
+                            case 4:
+                                loop_1 = false;
+                                break;
+    
+                            default:
+                                System.out.println("Don't fucking waste my time!");
+                                break;
+                        }
+                            
+                    }
+
+                    break;
         
                 case "Savings":
                     Boolean loop_2 = true;
