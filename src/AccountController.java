1a7f8e2f78c4a792b780a27cc60d407a4e3aa478
PXIONG001 <pengxiong001@gmail.com>
2021-12-18 21:30:34 -0600

    Controls the Account. Might remodify it.

---
+++ src/AccountController.java
@@ -0,0 +1,13 @@
+/**
+ * Account.java is the class for the account. 
+ * 
+ * @author Pengshing Xiong
+ * @version 1.0
+ * @since 2021-12-14
+ */
+public class AccountController {
+    public static void main(String[] args) 
+    {
+        //Account account = new Account();
+    }
+}
