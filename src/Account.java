b605ddbd9c47024b52766b89dc8fdda9a849e6e2
PXIONG001 <pengxiong001@gmail.com>
2021-12-18 21:28:31 -0600

    Got rid of the constructor.

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 /**
  * Account.java is the class for the account. 
  * 
@@ -6,8 +8,17 @@
  * @since 2021-12-04
  */
 public class Account {
-    private String name;
+    private int id;
+    private String username;
     private String password;
+    private Date birthdate;
+    private String first_name;
+    private String last_name;
+    private int social_security;
+    private int routing_number;
+    private int account_number;
+    private double checking_balance;
+    private double savings_balance;
 
     /**
      * 
@@ -16,9 +27,34 @@ public class Account {
      * @param name The user's name
      * @param password The user's password
      */
-    public Account(String name, String password) {
-        this.name = name;
-        this.password = password;
+    // public Account(String id, String username, String password, String birthdate, String first_name, String last_name, String social_securty, String routing_number, String account_number, String checking_balance, String savings_balance) {
+    //     this.id = id;
+    //     this.username = username;
+    //     this.password = password;
+    //     this.birthdate = birthdate;
+    //     this.first_name = first_name;
+    //     this.last_name = last_name;
+    //     this.social_security = social_securty;
+    //     this.routing_number = routing_number;
+    //     this.account_number = account_number;
+    //     this.checking_balance = checking_balance;
+    //     this.savings_balance = savings_balance;
+    // }
+
+    /**
+     * 
+     * @return
+     */
+    public int getID() {
+        return id;
+    }
+
+    /**
+     * 
+     * @param id
+     */
+    public void setID(Integer id) {
+        this.id = id;
     }
 
     /**
@@ -28,7 +64,7 @@ public class Account {
      * @return Returns the user's name
      */
     public String getName() {
-        return name;
+        return username;
     }
 
     /**
@@ -37,8 +73,8 @@ public class Account {
      * 
      * @param name The user's name
      */
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String username) {
+        this.username = username;
     }
 
     /**
@@ -59,6 +95,134 @@ public class Account {
      */
     public void setPassword(String password) {
         this.password = password;
+    }   
+    
+    /**
+    * 
+    * @return
+    */
+   public Date getBirthdate() {
+       return birthdate;
+   }
+
+   /**
+    * 
+    * @param birthdate
+    */
+   public void setBirthdate(Date birthdate) {
+       this.birthdate = birthdate;
+   }
+
+    /**
+    * 
+    * @return
+    */
+    public String getFirstName() {
+        return first_name;
+    }
+ 
+    /**
+     * 
+     * @param first_name
+     */
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+    /**
+    * 
+    * @return
+    */
+    public String getLastName() {
+        return last_name;
+    }
+ 
+    /**
+     * 
+     * @param last_name
+     */
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    /**
+    * 
+    * @return
+    */
+    public int getSocialSecurity() {
+        return social_security;
+    }
+ 
+    /**
+     * 
+     * @param social_security
+     */
+    public void setSocialSecurity(Integer social_security) {
+        this.social_security = social_security;
+    }
+
+    /**
+    * 
+    * @return
+    */
+    public int getRoutingNumber() {
+        return routing_number;
+    }
+ 
+    /**
+     * 
+     * @param routing_number
+     */
+    public void setRoutingNumber(Integer routing_number) {
+        this.routing_number = routing_number;
+    }
+
+    /**
+    * 
+    * @return
+    */
+    public int getAccountNumber() {
+        return account_number;
+    }
+ 
+    /**
+     * 
+     * @param account_number
+     */
+    public void setAccountNumber(Integer account_number) {
+        this.account_number = account_number;
+    }
+
+    /**
+    * 
+    * @return
+    */
+    public double getCheckingBalance() {
+        return checking_balance;
+    }
+ 
+    /**
+     * 
+     * @param checking_balance
+     */
+    public void setCheckingBalance(Double checking_balance) {
+        this.checking_balance = checking_balance;
+    }
+
+    /**
+    * 
+    * @return
+    */
+    public double getSavingsBalance() {
+        return savings_balance;
+    }
+ 
+    /**
+     * 
+     * @param checking_balance
+     */
+    public void setSavingsBalance(Double savings_balance) {
+        this.savings_balance = savings_balance;
     }
 
 }
