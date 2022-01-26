ed64320fe8c9f5fc44df0bd47675c41825639910
PXIONG001 <pengxiong001@gmail.com>
2022-01-25 23:08:51 -0600

    The file is still being modified and implemented as of now. More to come later.

--- src/AccountController.java
+++ src/AccountController.java
@@ -11,163 +11,10 @@ import java.util.Date;
  * @version 1.0
  * @since 2021-12-14
  */
-public class AccountController {
-
-    // Accessing the Database
-    Database database = new Database();
+public class AccountController extends RequestController {
 
     // Boolean for login
     boolean statement = false;
-
-    /**
-     * 
-     * The function gives back a string array list
-     * 
-     * @param request_list it is requesting which list to give back
-     * @return returns a string array list
-     */
-    public ArrayList<String> request_string(String request_list)
-    {
-        ArrayList<String> requested = new ArrayList<>();
-        ArrayList<String> user_username = new ArrayList<>();
-        ArrayList<String> user_password = new ArrayList<>();
-        ArrayList<String> user_first_name = new ArrayList<>();
-          // for-each loop to create the array list from the accounts. 
-          for (Account obj : database.connect())
-          {
-              user_username.add(obj.getUsername());
-              user_password.add(obj.getPassword());
-          }      
-
-          switch(request_list)
-          {
-            case "username":
-                requested = user_username;
-                break;
-
-            case "password":
-                requested =  user_password;
-                break;
-                
-            case "first name":
-                requested = user_first_name;
-                break;
-
-            default:
-                break;
-          }
-
-          return requested;
-    }
-
-    /**
-     *  
-     * The function returns an integer array list
-     * 
-     * @param request_list which list to get
-     * @return returns an integer array list
-     */
-    public ArrayList<Integer> request_int(String request_list)
-    {
-        ArrayList<Integer> requested = new ArrayList<>();
-        ArrayList<Integer> user_id = new ArrayList<>();
-        ArrayList<Integer> user_social_security = new ArrayList<>();
-        ArrayList<Integer> user_routing_number = new ArrayList<>();
-        ArrayList<Integer> user_account_number = new ArrayList<>();
-
-          // for-each loop to create the array list from the accounts. 
-          for (Account obj : database.connect())
-          {
-              user_id.add(obj.getID());
-              user_social_security.add(obj.getSocialSecurity());
-              user_routing_number.add(obj.getRoutingNumber());
-              user_account_number.add(obj.getAccountNumber());
-          }      
-
-          switch(request_list)
-          {
-            case "id":
-                for (int i = 0; i < user_id.size(); i ++)
-                {
-                    System.out.println(user_id.get(i));
-                }
-                requested = user_id;
-                break;
-
-            case "password":
-                requested =  user_social_security;
-                break;
-                
-            case "first name":
-                requested = user_routing_number;
-                break;
-
-            case "last name":
-                requested = user_account_number;
-                break;
-
-            default:
-                break;
-          }
-
-          return requested;
-    }
-
-    /**
-     * 
-     * The function returns an array list for the checking and saving's account
-     * 
-     * @param request_list the name of which array list to obtain
-     * @return returns the requested array list.
-     */
-    public ArrayList<Double> request_double(String request_list)
-    {
-        ArrayList<Double> requested = new ArrayList<>();
-        ArrayList<Double> checking_balance = new ArrayList<>();
-        ArrayList<Double> savings_balance = new ArrayList<>();
-
-          // for-each loop to create the array list from the accounts. 
-          for (Account obj : database.connect())
-          {
-              checking_balance.add(obj.getCheckingBalance());
-              savings_balance.add(obj.getSavingsBalance());
-          }      
-
-
-        switch(request_list)
-        {
-            case "checking account":
-                requested = checking_balance;
-                break;
-
-            case "savings account":
-                requested = savings_balance;
-                break;
-
-            default:
-                break;
-        }
-
-        return requested;
-    }
-
-    /**
-     *
-     * The function returns an array list of dates.
-     * 
-     * @return Returns an arraylist of dates.
-     */
-    public ArrayList<Date> request_date()
-    {
-        ArrayList<Date> date = new ArrayList<>();
-
-        for (Account obj : database.connect())
-        {
-            date.add(obj.getBirthdate());
-        }
-
-        return date;
-    }
 
     /**
      * 
@@ -198,18 +45,7 @@ public class AccountController {
         
     }
 
-    /**
-     * 
-     * This function updates the balance of the checking or savings account.
-     * 
-     * @param money
-     * @param type_of_account
-     * @param username_account
-     */
-    public void updating_user_balance(double money, String type_of_account, String username_account)
-    {
-        database.update_balance_information(money, type_of_account, username_account);
-    }
+
 
     /**
      * 
