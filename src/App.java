ff9df39f1acd7ae6fe63cfbf9013b2a5937eec90
PXIONG001 <pengxiong001@gmail.com>
2022-01-25 23:08:41 -0600

    The file is still being modified and implemented as of now. More to come later.

--- src/App.java
+++ src/App.java
@@ -11,7 +11,24 @@ import java.util.*;
  */
 public class App extends AccountController
 {
+
+    /**
+     * 
+     * 
+     */
+    public void checking() 
+    {
+
+    }
     
+    /**
+     * 
+     * 
+     */
+    public void savings()
+    {
+
+    }
     /**
      * This is the main method that will control the ATM/Banking system.
      * 
@@ -20,13 +37,6 @@ public class App extends AccountController
     public static void main(String[] args) 
     {
         App application = new App();
-
-        // The array list variables for the strings
-        ArrayList<String> first_name = application.request_string("first name");
-        ArrayList<String> last_name = application.request_string("last name");
-
-        
-        
 
         // User input variable used in nest swtch cases
         String user_input_for_account;
@@ -46,160 +56,7 @@ public class App extends AccountController
         if (application.check_username(check_the_username) && application.check_password(check_the_password))
         {
             System.out.println("Welcome " + first_name.get(application.index_number(check_the_username)) + " " + last_name.get(application.index_number(check_the_username)) + " What would you like to do today?\n");
-            // The first continous loop for the user
-            while (loop) 
-            {
-
-                System.out.println("Checking\n\nSavings\n\nExit\n");
-                String user_input = sc.next();
-
-                // Switch case for accessing different accounts.
-                switch (user_input) {
-
-                    // Checking
-                    case "1":
-                    case "checking":
-                    case "Checking":
-                        Boolean checking_loop = true;
-                        
-                        // Nested loop. The nested continous loop keeps asking the users what they want to do
-                        // until they input the command: Exit.
-                        while (checking_loop) 
-                        {
-                            ArrayList<Double> checking_account = application.request_double("checking account");
-
-                            System.out.println("What would you like to do today?\n");
-                            System.out.println("1. Withdraw\n\n2. Deposit\n\n3. See Balance\n\n4. Exit\n");
-                            System.out.println("Type in the number:");
-                        
-                            user_input_for_account = sc.next();
-
-                            // Nest Switch case. The user gives an input on which action they would like to
-                            // proceed in their checking account
-                            switch (user_input_for_account) 
-                            {
-                                
-                                // Withdraw
-                                case "1":
-                                    double withdraw_checking_balance = checking_account.get(application.index_number(check_the_username));
-                                    System.out.println("How much would you like to take out from your account?\n");
-                                    double withdraw_amount = sc.nextDouble();
-                                    double withdraw = withdraw_checking_balance - withdraw_amount;
-                                    System.out.println("You have withdrawn $" + withdraw_amount + " from your checking account.");
-                                    application.updating_user_balance(withdraw, "checking account", check_the_username);
-                                    break;
-                                
-                                // Deposit
-                                case "2":
-                                    double deposit_checking_balance = checking_account.get(application.index_number(check_the_username));
-                                    System.out.println("How much would you like to put in your account?\n");
-                                    double deposit_amount = sc.nextDouble();
-                                    double deposit = deposit_checking_balance + deposit_amount;
-                                    System.out.println("You have deposited $" + deposit_amount + " from your checking account.");
-                                    application.updating_user_balance(deposit, "checking account", check_the_username);
-                                    break;
-        
-                                // See Balance
-                                case "3":
-                                    double checking_balance = checking_account.get(application.index_number(check_the_username));
-                                    System.out.println("You have $" + checking_balance + " in your checking account.");
-                                    break;
-
-                                // Exit
-                                case "4":
-                                    checking_loop = false;
-                                    break;
-        
-                                // Ask user to type in one of the following commands
-                                default:
-                                    System.out.println("Invalid input! Please type in a number to proceed: \n");
-                                    break;
-                            }
-                                
-                        }
-
-                        break;
-                    
-                    // Savings
-                    case "2":
-                    case "savings":
-                    case "Savings":
-                        Boolean savings_loop = true;
-                        
-                        // Nested loop. The nested continous loop keeps asking the users what they want to do
-                        // until they input the command: Exit.
-                        while (savings_loop) 
-                        {
-                            ArrayList<Double> savings_account = application.request_double("savings account");
-
-                            System.out.println("What would you like to do today?\n");
-                            System.out.println("1. Withdraw\n\n2. Deposit\n\n3. See Balance\n\n4. Exit\n");
-                            System.out.println("Type in the number:");
-                        
-                            user_input_for_account = sc.next();
-
-                            // Nest Switch case. The user gives an input on which action they would like to
-                            // proceed in their savings account
-                            switch (user_input_for_account) 
-                            {
-                                // Withdraw
-                                case "1":
-                                    double withdraw_savings_balance = savings_account.get(application.index_number(check_the_username));
-                                    System.out.println("How much would you like to take out from your account?\n");
-                                    double withdraw_amount = sc.nextDouble();
-                                    double withdraw = withdraw_savings_balance - withdraw_amount;
-                                    System.out.println("You have withdrawn $" + withdraw_amount + " from your checking account.");
-                                    application.updating_user_balance(withdraw, "savings account", check_the_username);
-                                    break;
-                                
-                                // Deposit
-                                case "2":
-                                    double deposit_savings_balance = savings_account.get(application.index_number(check_the_username));
-                                    System.out.println("How much would you like to put in your account?\n");
-                                    double deposit_amount = sc.nextDouble();
-                                    double deposit = deposit_savings_balance + deposit_amount;
-                                    System.out.println("You have deposited $" + deposit_amount + " from your checking account.");
-                                    application.updating_user_balance(deposit, "savings account", check_the_username);
-                                    break;
-        
-                                // See Balance
-                                case "3":
-                                    double savings_balance = savings_account.get(application.index_number(check_the_username));
-                                    System.out.println("You have $" + savings_balance + " in your checking account.");
-                                    break;
-
-                                // Exit
-                                case "4":
-                                    savings_loop = false;
-                                    break;
-        
-                                // Ask user to type in one of the following commands
-                                default:
-                                    System.out.println("Invalid input! Please type in a number to proceed: \n");
-                                    break;
-                            }
-                                
-                        }
-
-                        break;
-                    
-                    // Exit
-                    case "3":
-                    case "exit":
-                    case "Exit":
-                        loop = false;
-                        break;
-
-                    // If the user does not type in any of the inputs
-                    default:
-                        System.out.println("Invalid input! Please type in the following input: ");
-                        break;
-                }
-
-            }
             
-            // Closes the scanner
-            sc.close();
         }
 
         else
